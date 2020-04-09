470ec5d23ac43b581be2c5551d1893f2a3d2ce19
rina-1 <cba.mac@icloud.com>
2020-04-10 01:15:01 +0900

    [Finish]3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -28,6 +28,13 @@ def update
 	redirect_to todolist_path(list.id)
 end
 
+def destroy
+  list = List.find(params[:id])
+  list.destroy
+  redirect_to todolists_path
+end
+
+
 private
 
  def list_params
